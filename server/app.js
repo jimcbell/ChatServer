803fb666ea79dad94f521db0d8dd4df826766a15
Jim Campbell <jimcampbell355@gmail.com>
2025-08-18 22:08:39 -0400

    server updates

--- server/app.js
+++ server/app.js
@@ -1,9 +1,27 @@
 import express from "express";
 
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-export { app };
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected:", socket.id);
+  socket.on("disconnect", () => {
+    console.log("user disconnected: ", socket.id);
+  });
+});
+
+export { server };
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-import { app } from "./app.js";
+import { server } from "./app.js";
 
 try {
-  app.listen(3000, () => {
+  server.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
   });
 } catch (error) {
